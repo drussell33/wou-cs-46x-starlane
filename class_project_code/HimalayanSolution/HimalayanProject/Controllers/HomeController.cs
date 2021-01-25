52a4d40b4068ce3fabd07dd37ec0f6aeb0faff8c
Rafael Arellano <Rarellano05@wou.edu>
2021-01-25 00:54:50 -0800

    registered dbcontect service for 'Expeditions' dbcontext in startup.cs, created dbcontext variable for use in HomeController as well as 'Search' controller method in HomeController.

--- class_project_code/HimalayanSolution/HimalayanProject/Controllers/HomeController.cs
+++ class_project_code/HimalayanSolution/HimalayanProject/Controllers/HomeController.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using HimalayanProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HimalayanProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private HimalayanContext db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, HimalayanContext context)
         {
             _logger = logger;
+            db = context;
         }
 
         public IActionResult Index()
@@ -32,6 +35,14 @@ namespace HimalayanProject.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Search(string search_term)
+        {
+
+
+
+            return View();
         }
     }
 }
