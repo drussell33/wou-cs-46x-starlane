330f71d134b69817999c5418d4650abb6cc8d796
Derek Russell PC <drussell19@wou.edu>
2021-02-22 01:57:58 -0800

    small changes to test a theory

---
+++ main_project_code/TeamProject/iCollections/wwwroot/js/homepage.js
@@ -0,0 +1,33 @@
+﻿THREE = importScripts("~/js/three.module.js");
+
+function main() {
+    var canvas = document.querySelector("#c");
+    var renderer = new THREE.WebGLRenderer({ canvas });
+
+    var fov = 75;
+    var aspect = 2;  // the canvas default
+    var near = 0.1;
+    var far = 5;
+    var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
+
+    var scene = new THREE.Scene();
+
+    var boxWidth = 1;
+    var boxHeight = 1;
+    var boxDepth = 1;
+    var geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+    var material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });  // greenish blue
+
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    renderer.render(scene, camera);
+}
+
+$("#c").ready( function () {
+    main();
+});
+
+
