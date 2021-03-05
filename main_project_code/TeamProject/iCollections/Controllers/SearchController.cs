a99617a4113b4d4c9843fbac89c0f418d4370a5d
Cuauhtemoc Reilly <cuauhtemoc.reilly.8178@mail.linnbenton.edu>
2021-03-05 00:51:39 -0800

    Added rough search function.

---
+++ main_project_code/TeamProject/iCollections/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using iCollections.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using iCollections.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+namespace iCollections.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ILogger<SearchController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ICollectionsDbContext _db;
+
+        public SearchController(ILogger<SearchController> logger, UserManager<IdentityUser> userManager, ICollectionsDbContext db)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public IActionResult SearchUser(string user)
+        {
+            IcollectionUser loggedInUser = null;
+            if(User.Identity.IsAuthenticated)
+            {
+                loggedInUser = _db.IcollectionUsers.FirstOrDefault(x => x.AspnetIdentityId == _userManager.GetUserId(User));
+            }
+            List<IcollectionUser> results = _db.IcollectionUsers.Where(x => x.UserName.Contains(user)).ToList();
+            return View(new SearchList { loggedInUser = loggedInUser, results = results });
+        }
+    }
+}
