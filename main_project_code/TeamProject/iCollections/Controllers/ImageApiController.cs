df4ac5eadf67f2d78f77c5f8816ef190aa228619
Baltazar Ortiz <bortiz19@mail.wou.edu>
2021-03-30 16:54:52 -0700

    Making image api endpoint

---
+++ main_project_code/TeamProject/iCollections/Controllers/ImageApiController.cs
@@ -0,0 +1,21 @@
+using System;
+using iCollections.Data;
+using iCollections.Models;
+using System.IO;
+
+namespace iCollections.Controllers
+{
+    // All this class does is retrieves photo(s)
+    public class ImageApiController
+    {
+        public ImageApiController()
+        {
+
+        }
+
+        public Photo Thumbnail() {
+            return null;
+        }
+
+    }
+}
