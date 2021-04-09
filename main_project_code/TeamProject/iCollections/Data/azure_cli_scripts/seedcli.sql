57c0a20536b56bc2309dfeee4b075b9ae0f4eabb
Derek Russell PC <drussell19@wou.edu>
2021-04-09 03:13:40 -0700

    updated the azure seed scripts to get the live demo back up

--- main_project_code/TeamProject/iCollections/Data/azure_cli_scripts/seedcli.sql
+++ main_project_code/TeamProject/iCollections/Data/azure_cli_scripts/seedcli.sql
@@ -25,27 +25,27 @@ INSERT INTO [Follow] (follower,followed, began) VALUES
 	(4,3, '05/13/2015 5:50 AM');
 
 
-INSERT INTO [Collection](name,visibility,user_id,date_made) VALUES
-	('Collection1', 1, '1', '05/29/2015 5:50 AM'),
-	('My Fish', 1, '1', '05/28/2015 5:50 AM'),
-	('My Beer', 1, '1', '05/27/2015 5:50 AM'),
-	('My Tools', 1, '1', '05/26/2015 5:50 AM'),
-	('My Friends', 1, '1', '05/25/2015 5:50 AM'),
-	('Collection2', 1, '2', '05/24/2015 5:50 AM'),
-	('My Trophyies', 1, '2', '05/23/2015 5:50 AM'),
-	('My Plants', 1, '2', '05/22/2015 5:50 AM'),
-	('My Cards', 1, '2', '05/21/2015 5:50 AM'),
-	('My Games', 1, '2', '05/20/2015 5:50 AM'),
-	('Collection3', 1, '3', '05/19/2015 5:50 AM'),
-	('My Stickers', 1, '3', '05/18/2015 5:50 AM'),
-	('My Stamps', 1, '3', '05/17/2015 5:50 AM'),
-	('My Posters', 1, '3', '05/16/2015 5:50 AM'),
-	('My Funco Pops', 1, '3', '05/15/2015 5:50 AM'),
-	('Collection4', 1, '4', '05/14/2015 5:50 AM'),
-	('My Toenails', 1, '4', '05/13/2015 5:50 AM'),
-	('My Furniture', 1, '4', '05/12/2015 5:50 AM'),
-	('My statues', 1, '4', '05/11/2015 5:50 AM'),
-	('My Vases', 1, '4', '05/10/2015 5:50 AM');
+INSERT INTO [Collection](name,visibility,user_id,date_made, route) VALUES
+	('Collection1', 1, '1', '05/29/2015 5:50 AM', '~/ocean_environment'),
+	('My Fish', 1, '1', '05/28/2015 5:50 AM', '~/ocean_environment'),
+	('My Beer', 1, '1', '05/27/2015 5:50 AM', '~/ocean_environment'),
+	('My Tools', 1, '1', '05/26/2015 5:50 AM', '~/ocean_environment'),
+	('My Friends', 1, '1', '05/25/2015 5:50 AM', '~/ocean_environment'),
+	('Collection2', 1, '2', '05/24/2015 5:50 AM', '~/ocean_environment'),
+	('My Trophyies', 1, '2', '05/23/2015 5:50 AM', '~/ocean_environment'),
+	('My Plants', 1, '2', '05/22/2015 5:50 AM', '~/ocean_environment'),
+	('My Cards', 1, '2', '05/21/2015 5:50 AM', '~/ocean_environment'),
+	('My Games', 1, '2', '05/20/2015 5:50 AM', '~/ocean_environment'),
+	('Collection3', 1, '3', '05/19/2015 5:50 AM', '/gallery_environment'),
+	('My Stickers', 1, '3', '05/18/2015 5:50 AM', '/gallery_environment'),
+	('My Stamps', 1, '3', '05/17/2015 5:50 AM', '/gallery_environment'),
+	('My Posters', 1, '3', '05/16/2015 5:50 AM', '/gallery_environment'),
+	('My Funco Pops', 1, '3', '05/15/2015 5:50 AM', '/gallery_environment'),
+	('Collection4', 1, '4', '05/14/2015 5:50 AM', '/gallery_environment'),
+	('My Toenails', 1, '4', '05/13/2015 5:50 AM', '/gallery_environment'),
+	('My Furniture', 1, '4', '05/12/2015 5:50 AM', '/gallery_environment'),
+	('My statues', 1, '4', '05/11/2015 5:50 AM', '/gallery_environment'),
+	('My Vases', 1, '4', '05/10/2015 5:50 AM', '/gallery_environment');
 
 
 INSERT INTO [Photo](name, user_id, date_uploaded) VALUES
@@ -72,4 +72,4 @@ INSERT INTO [FriendsWith](user1_id, user2_id, began) VALUES
 	('3','2', '05/23/2015 5:50 AM'),
 	('3','4', '05/21/2015 5:50 AM'),
 	('4','1', '05/25/2015 5:50 AM'),
-	('4','3', '05/21/2015 5:50 AM');
+	('4','3', '05/21/2015 5:50
