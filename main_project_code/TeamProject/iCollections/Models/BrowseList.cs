e69957a33ab79da7f9901b09e338eb7a8b369eed
Rafael Arellano <Rarellano05@wou.edu>
2021-04-09 03:44:51 -0700

    Refined browse view further and created a 'BrowseList' view model that will be used by the BrowseController to pass to the Browse view.

---
+++ main_project_code/TeamProject/iCollections/Models/BrowseList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iCollections.Models
+{
+    public class BrowseList
+    {
+        public IcollectionUser loggedInUser { get; set; }
+
+        public List<Collection> searchResults { get; set; }
+
+        public List<CollectionKeyword> suggestedKeywords { get; set; }
+
+    }
+}
