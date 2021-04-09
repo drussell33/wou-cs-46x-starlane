8f5770e3f0a1eaeeaad1860b2c470cd0775801f1
Derek Russell PC <drussell19@wou.edu>
2021-04-09 01:56:06 -0700

    reconnected to the 2 database that I put on azure for the first live demo

--- main_project_code/TeamProject/iCollections/Models/Photo.cs
+++ main_project_code/TeamProject/iCollections/Models/Photo.cs
@@ -18,8 +18,9 @@ namespace iCollections.Models
         public byte[] Data { get; set; }
         public int? UserId { get; set; }
         public DateTime? DateUploaded { get; set; }
-        //public id PhotoGUID { get; set; }
-
+        //added in sprint 3
+        public Guid PhotoGuid { get; set; }
+        //SQL version
         //[PhotoGUID] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID()
 
         public virtual IcollectionUser User { get; set; }
