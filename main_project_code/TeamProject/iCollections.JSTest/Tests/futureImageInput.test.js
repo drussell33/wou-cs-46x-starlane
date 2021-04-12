987ded921913723165f4c209b9bed85399aeeaa0
Derek Russell PC <drussell19@wou.edu>
2021-04-12 02:50:14 -0700

    set up testing project and dependencies for the javascript unit testing with jest. practiced with a lot of the sample tests with jest, then doing my part of thre user story in the fuctureimageinput tests, that is currently being done seperately in the practice sum.js file

---
+++ main_project_code/TeamProject/iCollections.JSTest/Tests/futureImageInput.test.js
@@ -0,0 +1,101 @@
+import {practiceInit} from '../../iCollections/wwwroot/js/sum'
+//const sum = require('./sum');
+
+test('CanvasObject_ShouldCreateAndModify_True', () => {
+    //Arrange
+    var canvas = document.createElement('canvas');
+    //Act
+    canvas.width = 150;
+    //Assert
+    expect(150).toBe(canvas.width);
+});
+
+test('ImageObject_ShouldCreateAndModify_True', () => {
+    //Arrange
+    var imageObj = new Image();
+    imageObj.src = './images/fish_pics/fish4.png';
+    //Act
+    imageObj.width = 500;
+    //Assert
+    expect(500).toBe(imageObj.width);
+});
+
+
+const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'beer',
+  ];
+  
+  test('JestSampleArrayReading', () => {
+    expect(shoppingList).toContain('beer');
+    expect(new Set(shoppingList)).toContain('beer');
+  });
+
+  let sampleData = [
+    {
+        "Data": "notreal0",
+        "Title": "title0",
+        "PhotoRank": 0,
+        "Description": "description0"
+    },
+    {
+        "Data": "notreal1",
+        "Title": "title1",
+        "PhotoRank": 1,
+        "Description": "description1"
+    },
+    {
+        "Data": "notreal2",
+        "Title": "title2",
+        "PhotoRank": 2,
+        "Description": "description2"
+    },
+    {
+        "Data": "notreal3",
+        "Title": "title3",
+        "PhotoRank": 3,
+        "Description": "description3" 
+    },
+    {
+        "Data": "notreal4",
+        "Title": "title4",
+        "PhotoRank": 4,
+        "Description": "description4"
+    },
+    {
+        "Data": "notreal5",
+        "Title": "title5",
+        "PhotoRank": 5,
+        "Description": "description5"
+    },
+    {
+        "Data": "notreal6",
+        "Title": "title6",
+        "PhotoRank": 6,
+        "Description": "description6"
+    },
+    {
+        "Data": "notreal7",
+        "Title": "title7",
+        "PhotoRank": 7,
+        "Description": "description7"
+    },
+    ];
+    
+    // function practiceInit(sampleData)
+    // {
+    //     var copyData = sampleData
+    //     return copyData;
+    // }   
+
+  test('CreatingArrayParam_SHouldLoadArrayAsParamToFunction_True', () => {
+    //Arrange
+    var inputParam = practiceInit(sampleData);
+    //Act
+    var testLength = inputParam.length;
+    //Assert
+    expect(8).toBe(testLength);
+});
