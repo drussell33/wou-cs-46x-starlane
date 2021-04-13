b6eca26e70a3ddf96de265bfa3542f74d4e4be2b
Rafael Arellano <Rarellano05@wou.edu>
2021-04-13 01:30:39 -0700

    Refactored code from Index method in BrowseController that parsed user search by white space into array. Created 'StringUtilities' class in utilities folder where 'SplitBySpace' method was created to parse strings by space.

---
+++ main_project_code/TeamProject/iCollections/Utilities/StringUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iCollections.Utilities
+{
+    public class StringUtilities
+    {
+
+        // Refactored code from Browse Controller for Sprint 3 testing
+        public static string[] SplitBySpace(string keywords)
+        {
+            string[] keywordArray = keywords.Split(" ");
+
+            return (keywordArray);
+        }
+    }
+}
