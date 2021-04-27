e556eb9ef85fd3a4edceae75585a36d6598e5d92
Cuauhtemoc Reilly <cuauhtemoc.reilly.8178@mail.linnbenton.edu>
2021-04-27 02:15:24 -0700

    Added error view.

---
+++ main_project_code/TeamProject/iCollections/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iCollections.Models;
+
+namespace iCollections.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index(ErrorMessage error)
+        {
+            return View(error);
+        }
+    }
+}
---
+++ main_project_code/TeamProject/iCollections/Data/Abstract/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iCollections.Models;
+
+namespace iCollections.Data.Abstract
+{
+    public interface IUserRepository : IRepository<IcollectionUser>
+    {
+        IcollectionUser GetUserByIdentityId(string identityID);
+        bool Exists(IcollectionUser user);
+        IcollectionUser GetUser(int id);
+    }
+}
