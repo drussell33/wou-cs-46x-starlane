1016a409335b60ac746da8709cd775c8f2a1f5af
Baltazar Ortiz <bortiz19@mail.wou.edu>
2021-04-28 18:05:41 -0700

    Adding more tests

---
+++ main_project_code/TeamProject/iCollections.Tests/Tests/TestStuff.cs
@@ -0,0 +1,24 @@
+// Baltazar Ortiz
+
+using NUnit.Framework;
+using iCollections.Controllers;
+using iCollections.Models;
+using Microsoft.AspNetCore.Mvc;
+using iCollections.Data.Abstract;
+using Moq;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading;
+using System.Security.Claims;
+
+namespace iCollections.Tests.Tests
+{
+    public class TestStuff
+    {
+        [SetUp]
+        public void Setup() {}
+
+        public void Test
+    }
+}
