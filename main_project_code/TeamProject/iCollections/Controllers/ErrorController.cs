5c08d7f20ebfcae7792a855ce87eb81962327be5
Baltazar Ortiz <bortiz19@mail.wou.edu>
2021-05-19 16:33:52 -0700

    Added custom login page WIP

--- main_project_code/TeamProject/iCollections/Controllers/ErrorController.cs
+++ main_project_code/TeamProject/iCollections/Controllers/ErrorController.cs
@@ -13,5 +13,10 @@ namespace iCollections.Controllers
         {
             return View(error);
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
